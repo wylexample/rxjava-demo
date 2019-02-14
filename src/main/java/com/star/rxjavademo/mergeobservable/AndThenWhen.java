0cb1dcb3086834393c043a8690142e47a04729a2
wuyulong <wuyulong@vipkid.com.cn>
2019-02-14 16:03:59 +0800

    merge start

---
+++ src/main/java/com/star/rxjavademo/mergeobservable/AndThenWhen.java
@@ -0,0 +1,13 @@
+package com.star.rxjavademo.mergeobservable;
+
+/**
+ * @author wuyulong
+ * @date 2019/2/14
+ * @desc 使用Pattern和Plan作为中介，将两个或多个Observable发射的数据集合并到一起 AndThenWhen/
+ */
+public class AndThenWhen {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/star/rxjavademo/mergeobservable/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author wuyulong
+ * @date 2019/2/14
+ * @desc 结合observable的相关操作符
+ */
+package com.star.rxjavademo.mergeobservable;
